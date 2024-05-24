388babe5abb6353193a4f34e23182e762971717c
Kieru <jdawggseventy@gmail.com>
2024-05-25 02:30:23 +1000

    Initial commit

---
+++ src/main/java/net/kieru/toozium/TooziumClient.java
@@ -0,0 +1,10 @@
+package net.kieru.toozium;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TooziumClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/kieru/toozium/TooziumDataGenerator.java
@@ -0,0 +1,11 @@
+package net.kieru.toozium;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TooziumDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
